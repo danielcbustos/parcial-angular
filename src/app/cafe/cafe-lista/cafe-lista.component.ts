b14470b76eefef0ae9dd3a091d32c7a64ed67707
danilcbustos <d.bustosr@uniandes.edu.co>
2022-11-04 01:56:04 -0500

    Calculo variedades de cafe

--- src/app/cafe/cafe-lista/cafe-lista.component.ts
+++ src/app/cafe/cafe-lista/cafe-lista.component.ts
@@ -10,13 +10,34 @@ import { CafeService } from '../cafe.service';
 export class CafeListaComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  totalCafeOrigen: number = 0;
+  totalCafeBlend: number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void {
     this.cafeService.getCafes().subscribe((cafes) => {
       this.cafes = cafes;
+      this.calcularTotalCafeOrigen(cafes);
+      this.calcularTotalCafeBlend(cafes);
     });
+  }
+  calcularTotalCafeOrigen(cafes: Array<Cafe>): number {
+    for (let cafe of cafes) {
+      if (cafe.tipo == 'Café de Origen') {
+        this.totalCafeOrigen++;
+      }
+    }
+    return this.totalCafeOrigen;
+  }
+
+  calcularTotalCafeBlend(cafes: Array<Cafe>): number {
+    for (let cafe of cafes) {
+      if (cafe.tipo == 'Blend') {
+        this.totalCafeBlend++;
+      }
+    }
+    return this.totalCafeBlend;
   }
 
   ngOnInit() {
